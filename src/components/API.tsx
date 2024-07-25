d97a5c1aae11445338b1f8519030bd46558c9388
datots <dato_ts@hotmail.com>
2024-07-25 19:12:42 +0400

    registration form updated

--- src/components/API.tsx
+++ src/components/API.tsx
@@ -1,7 +1,6 @@
 import axios from "axios";
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import ReactPaginate from "react-paginate";
 import Posts from "./Posts";
 import Pagination from "./Pagination";
 const FetchData = () => {
--- src/components/Posts.tsx
+++ src/components/Posts.tsx
@@ -1,3 +1,11 @@
+import {
+  Key,
+  ReactElement,
+  JSXElementConstructor,
+  ReactNode,
+  ReactPortal,
+} from "react";
+
 const Posts = ({ posts, loading }) => {
   if (loading) {
     return <h1>Loading...</h1>;
@@ -5,15 +13,37 @@ const Posts = ({ posts, loading }) => {
   return (
     <div>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg: grid-cols=4 gap-4 mt-14">
-        {posts.map((post) => (
-          <div
-            key={post.id}
-            className="bg-white p-4 rounded-lg shadow-md transform transition-transform duration-400 hover:scale-110"
-          >
-            <h2 className="text-xl font-semibold mb-2">{post.title}</h2>
-            <p>{post.body}</p>
-          </div>
-        ))}
+        {posts.map(
+          (post: {
+            id: Key | null | undefined;
+            title:
+              | string
+              | number
+              | boolean
+              | ReactElement<any, string | JSXElementConstructor<unknown>>
+              | Iterable<ReactNode>
+              | ReactPortal
+              | null
+              | undefined;
+            body:
+              | string
+              | number
+              | boolean
+              | ReactElement<any, string | JSXElementConstructor<any>>
+              | Iterable<ReactNode>
+              | ReactPortal
+              | null
+              | undefined;
+          }) => (
+            <div
+              key={post.id}
+              className="bg-white p-4 rounded-lg shadow-md transform transition-transform duration-400 hover:scale-110"
+            >
+              <h2 className="text-xl font-semibold mb-2">{post.title}</h2>
+              <p>{post.body}</p>
+            </div>
+          )
+        )}
       </div>
     </div>
   );
